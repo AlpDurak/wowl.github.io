e4822b8644bede91bdbde96824dfe3592419984d
buzz <68165698+AlpDurak@users.noreply.github.com>
2024-01-22 17:40:18 +0300

    new stuff

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,16 +3,9 @@ const $scrollIndicator = document.querySelector(".scrollDown");
 // Fivem Section
 const $fivemSection = document.querySelector("section.fivem");
 const $fivemBackground = document.querySelector("section.bg-green");
-const fivemStyle = getComputedStyle($fivemBackground);
-const bgData = {
-  w: parseInt(fivemStyle.getPropertyValue("--w")),
-  h: parseInt(fivemStyle.getPropertyValue("--h")),
-  o: parseFloat(fivemStyle.getPropertyValue("--o")),
-};
-
-console.log(JSON.stringify(bgData));
 
 window.addEventListener("scroll", () => {
+  // Scroll Indicator
   if (window.scrollY > 200) {
     $scrollIndicator.style.opacity = 0;
   } else {
@@ -20,22 +13,11 @@ window.addEventListener("scroll", () => {
   }
 
   // FiveM Section
-  const growPercent = 50;
   if (window.scrollY > 300) {
     $fivemSection.style.paddingTop = "0px";
-    $fivemBackground.style.setProperty(
-      "--w",
-      `${bgData.w + (bgData.w * growPercent) / 100}px`
-    );
-    $fivemBackground.style.setProperty(
-      "--h",
-      `${bgData.h + (bgData.h * growPercent) / 100}px`
-    );
-    $fivemBackground.style.setProperty("--o", (bgData.o + 0.5).toString());
+    $fivemBackground.classList.add("active")
   } else if ($fivemSection.style.paddingTop === "0px") {
     $fivemSection.style.paddingTop = "200px";
-    $fivemBackground.style.setProperty("--w", `${bgData.w}px`);
-    $fivemBackground.style.setProperty("--h", `${bgData.h}px`);
-    $fivemBackground.style.setProperty("--o", bgData.o.toString());
+    $fivemBackground.classList.remove("active")
   }
 });
